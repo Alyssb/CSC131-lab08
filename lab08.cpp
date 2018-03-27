6ddae2b6fc64585043b2c11c91bc05c5aeae2ebf
Alyssb <ajs41@missouristate.edu>
2018-03-27 13:23:53 -0500

    all inputs made.

--- lab08.cpp
+++ lab08.cpp
@@ -17,10 +17,22 @@ double calculateRetail(double wholesale, double markup);
 //main has to be an int because you return the exit sucess sequence
 int main() {
 
+    double wholesale{};
+    double markup{};
+    double final{};
+
     std::cout << "what are you buying?? ";
-    std::string item;
+    std::string item{};
     std::getline(std::cin, item);
     std::cout << "you're getting a " << item << ", wOw" << std::endl;
+    std::cout << "og price?? ";
+    std::cin >> wholesale;
+    std::cout << "WOW!! " << wholesale << "?? what a low price!!" << std::endl;
+    std::cout << "what's the markup tho??";
+    std::cin >> markup;
+    std::cout << markup << "%? now that's just ridiculous smh" << std::endl;
+
+
     return EXIT_SUCCESS;
 }
 
