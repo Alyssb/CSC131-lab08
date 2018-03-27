2a6564093783d334f61af99358b5966bc7d1a5b6
Alyssb <ajs41@missouristate.edu>
2018-03-27 13:03:51 -0500

    added main and function

--- lab08.cpp
+++ lab08.cpp
@@ -7,9 +7,24 @@
  */
 
 // TODO: Include any and all libraries needed by your program
+#include <cstdlib> //c standard library, needed for all std lines
+#include <iostream> //for cout and cin, all user inputs
+#include <string> //to be able to use any sort of string
 
 double calculateRetail(double wholesale, double markup);
 
 // TODO: Write main below
+//main has to be an int because you return the exit sucess sequence
+int main() {
 
-// TODO: Implement your calculateRetail function below
+    string item;
+    std::cout << "what are you buying?? ";
+    std::cin >> number;
+    return EXIT_SUCCESS;
+}
+
+double calculateRetail(double wholesale, double markup) {
+
+    return 0;
+
+}
