4ac3578afbb79d6eff081125d3710636844df1ac
Alyssb <ajs41@missouristate.edu>
2018-03-27 12:50:42 -0500

    update header

--- lab08.cpp
+++ lab08.cpp
@@ -3,8 +3,7 @@
  * Missouri State University
  * 
  * @file lab08.cpp
- * TODO: Modify the next line accordingly and erase this line
- * @author Your Name <your_email@live.missouristate.edu>
+ * @author Alyssa Slayton <ajs41@missouristate.edu>
  */
 
 // TODO: Include any and all libraries needed by your program
