3f5b5c564e44d1f0d0096c424e64e79b566c33b6
Alyssb <ajs41@missouristate.edu>
2018-03-27 13:16:34 -0500

    get and return item description

--- lab08.cpp
+++ lab08.cpp
@@ -17,9 +17,10 @@ double calculateRetail(double wholesale, double markup);
 //main has to be an int because you return the exit sucess sequence
 int main() {
 
-    string item;
     std::cout << "what are you buying?? ";
-    std::cin >> number;
+    std::string item;
+    std::getline(std::cin, item);
+    std::cout << "you're getting a " << item << ", wOw" << std::endl;
     return EXIT_SUCCESS;
 }
 
