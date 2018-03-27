3928ec83d09980a9c6c94dde6bc0e772eb8d1fe4
Alyssb <ajs41@missouristate.edu>
2018-03-27 13:37:14 -0500

    finished

--- lab08.cpp
+++ lab08.cpp
@@ -6,6 +6,7 @@
  * @author Alyssa Slayton <ajs41@missouristate.edu>
  */
 
+//all comments like this are for future self.
 #include <cstdlib> //c standard library, needed for all std lines
 #include <iostream> //for cout and cin, all user inputs
 #include <string> //to be able to use any sort of string
@@ -18,21 +19,30 @@ int main() {
     std::cout << "what are you buying?? ";
     std::string item{};
     std::getline(std::cin, item);
-    std::cout << "you're getting a " << item << "?, wOw!" << std::endl;
+    std::cout << "you're getting a "
+              << item
+              << "?, wOw!"
+              << std::endl;
 
     double wholesale{};
     std::cout << "og price?? ";
     std::cin >> wholesale;
-    std::cout << "WOW!! $" << wholesale << "?? what a low price!!" << std::endl;
+    std::cout << "WOW!! $"
+              << wholesale
+              << "?? what a low price!!"
+              << std::endl;
 
     double markup{};
     std::cout << "what's the markup tho?? ";
     std::cin >> markup;
-    std::cout << markup << "%? now that's just ridiculous smh" << std::endl;
+    std::cout << markup
+              << "%? now that's just ridiculous smh"
+              << std::endl;
 
     std::cout << "\nsry but that's gonna be "
               << calculateRetail(wholesale, markup)
-              << " dolors tyty." << std::endl;
+              << " dolors tyty."
+              << std::endl;
 
     return EXIT_SUCCESS;
 }
