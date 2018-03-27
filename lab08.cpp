a5c223bbd40074f8cd48d9c61a084f112bc96077
Alyssb <ajs41@missouristate.edu>
2018-03-27 13:34:45 -0500

    finished

--- lab08.cpp
+++ lab08.cpp
@@ -6,32 +6,33 @@
  * @author Alyssa Slayton <ajs41@missouristate.edu>
  */
 
-// TODO: Include any and all libraries needed by your program
 #include <cstdlib> //c standard library, needed for all std lines
 #include <iostream> //for cout and cin, all user inputs
 #include <string> //to be able to use any sort of string
 
 double calculateRetail(double wholesale, double markup);
 
-// TODO: Write main below
 //main has to be an int because you return the exit sucess sequence
 int main() {
-
-    double wholesale{};
-    double markup{};
-    double final{};
 
     std::cout << "what are you buying?? ";
     std::string item{};
     std::getline(std::cin, item);
-    std::cout << "you're getting a " << item << ", wOw" << std::endl;
+    std::cout << "you're getting a " << item << "?, wOw!" << std::endl;
+
+    double wholesale{};
     std::cout << "og price?? ";
     std::cin >> wholesale;
-    std::cout << "WOW!! " << wholesale << "?? what a low price!!" << std::endl;
+    std::cout << "WOW!! $" << wholesale << "?? what a low price!!" << std::endl;
+
+    double markup{};
     std::cout << "what's the markup tho?? ";
     std::cin >> markup;
     std::cout << markup << "%? now that's just ridiculous smh" << std::endl;
 
+    std::cout << "\nsry but that's gonna be "
+              << calculateRetail(wholesale, markup)
+              << " dolors tyty." << std::endl;
 
     return EXIT_SUCCESS;
 }
